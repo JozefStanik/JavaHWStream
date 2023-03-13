cdd1ae8af039450f23f0efd2414b14679c85e999
Jozef Stanik <jozef.stanik@ness.com>
2023-03-13 15:21:41 +0100

    HW part 1

--- src/com/main/MainClass.java
+++ src/com/main/MainClass.java
@@ -5,7 +5,8 @@ import java.util.stream.Stream;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+		Stream<Integer> s1 = Stream.iterate(1, A->A+1);
+		System.out.println(s1.limit(1000).filter(A->(A%3==0)).filter(A->A%5==0).filter(A->A%7!=0).mapToDouble(A->A).average());
 	}
 
 }
